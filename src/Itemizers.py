373fd3668ebc1adc85b1b957cec9f534cd0e1f8c
Martin Kuehl <martin.kuehl@gmail.com>
2009-05-23 16:21:56 +0200

    Add custom itemizers.
    Add itemizer classes via PyObjC and make them use the newly added resources.
    Use the itemizer classes in the xml declarations.
    Update the setuptools script.

--- src/Itemizers.py
+++ src/Itemizers.py
@@ -2,8 +2,43 @@
 # -*- coding: utf-8 -*-
 
 from Foundation import objc
+from Foundation import NSBundle
+from AppKit import NSImage
 
-class HaskellFuncItem(objc.lookUpClass('ESBaseItem')):
+def iconForName(klass, name):
+	"""Return the NSImage instance representing a `name` item."""
+	imgpath = NSBundle.bundleForClass_(klass).pathForResource_ofType_(name, 'png')
+	img = NSImage.alloc().initWithContentsOfFile_(imgpath)
+	img.autorelease()
+	return img
+
+class HaskellModuleItem(objc.lookUpClass('ESBaseItem')):
+	"""Itemizer for modules"""
+	
+	def isDecorator(self):
+		return True
+	
+	def image(self):
+		return iconForName(self.class__(), 'module')
+	
+
+class HaskellTypeItem(objc.lookUpClass('ESBaseItem')):
+	"""Itemizer for datatypes"""
+	
+	def isDecorator(self):
+		return True
+	
+	def image(self):
+		return iconForName(self.class__(), 'type')
+	
+	def isTextualizer(self):
+		return True
+	
+	def title(self):
+		return self.text().strip()
+	
+
+class HaskellFunctionItem(objc.lookUpClass('ESBaseItem')):
 	"""Itemizer for functions"""
 	pass
 
@@ -14,6 +49,6 @@ class HaskellCodeBlockItem(objc.lookUpClass('ESCodeBlockItem')):
 		return True
 	
 	def title(self):
-		return '%s %s' % (u'{…}', self.text())
+		return '%s %s' % (u'{…}', self.text().strip())
 		
 	
